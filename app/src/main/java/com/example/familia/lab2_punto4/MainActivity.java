9e0844799c63a86553f3c12a13cb9a43658f3a33
Carlojo4 <carlojo4@gmail.com>
2015-05-05 09:52:41 -0500

    Laboratorio 3 --- con estilo aunque no se ve mucho porque ya se le habia puesto anteriormente un fondo de pantalla, tres idiomas y un icono de la app, también con conservación del estado al girar la pantalla y dos layout (land y port).

--- app/src/main/java/com/example/familia/lab2_punto4/MainActivity.java
+++ app/src/main/java/com/example/familia/lab2_punto4/MainActivity.java
@@ -13,84 +13,81 @@ import java.lang.Object;
 
 
 public class MainActivity extends ActionBarActivity {
-    private int opc=0;
+    private int opc = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void method1(View view){
-        switch (view.getId()){
+    public void method1(View view) {
+        switch (view.getId()) {
             case (R.id.rcuad):
-                opc=1;
+                opc = 1;
                 break;
             case (R.id.rtriang):
-                opc=2;
+                opc = 2;
                 break;
             case (R.id.rcirc):
-                opc=3;
+                opc = 3;
                 break;
             case (R.id.rrect):
-                opc=4;
+                opc = 4;
                 break;
         }
     }
-    public void method(View view){
-        final EditText _base = (EditText)findViewById(R.id.ebase);
-        final EditText _altura = (EditText)findViewById(R.id.ealtura);
-        final EditText _radio = (EditText)findViewById(R.id.eradio);
-        final EditText _lado = (EditText)findViewById(R.id.elado);
-        final EditText result = (EditText)findViewById(R.id.eresult);
 
-        final String base1=_base.getText().toString();
-        final String altura1=_base.getText().toString();
-        final String radio1=_base.getText().toString();
-        final String lado1=_base.getText().toString();
-        double base,altura, radio, lado, area;
+    public void method(View view) {
+        final EditText _base = (EditText) findViewById(R.id.ebase);
+        final EditText _altura = (EditText) findViewById(R.id.ealtura);
+        final EditText _radio = (EditText) findViewById(R.id.eradio);
+        final EditText _lado = (EditText) findViewById(R.id.elado);
+        final EditText result = (EditText) findViewById(R.id.eresult);
+
+        final String base1 = _base.getText().toString();
+        final String altura1 = _base.getText().toString();
+        final String radio1 = _base.getText().toString();
+        final String lado1 = _base.getText().toString();
+        double base, altura, radio, lado, area;
 
         base = Double.valueOf(_base.getText().toString());
         altura = Double.valueOf(_altura.getText().toString());
         radio = Double.valueOf(_radio.getText().toString());
         lado = Double.valueOf(_lado.getText().toString());
-        if(base1.length()==0 || altura1.length()==0 || radio1.length()==0 || lado1.length()==0){
+        if (base1.length() == 0 || altura1.length() == 0 || radio1.length() == 0 || lado1.length() == 0) {
             result.setText("ingrese valores.");
-        }
-        else{
-            switch(opc){
+        } else {
+            switch (opc) {
                 case 1:
-                    if (lado1.length()==0){
+                    if (lado1.length() == 0) {
                         result.setText("no hay valores.");
-                    }
-                    else{
-                        area = lado*lado;
+                    } else {
+                        area = lado * lado;
                         result.setText(String.valueOf(area));
                     }
                     break;
                 case 4:
-                    if (base1.length()==0 ||altura1.length()==0){
+                    if (base1.length() == 0 || altura1.length() == 0) {
                         result.setText("no hay valores.");
-                    }
-                    else{
-                        area = base*altura;
+                    } else {
+                        area = base * altura;
                         result.setText(String.valueOf(area));
                     }
                     break;
                 case 2:
-                    if (base1.length()==0 ||altura1.length()==0){
+                    if (base1.length() == 0 || altura1.length() == 0) {
                         result.setText("no hay valores.");
-                    }
-                    else{
-                        area = (base*altura)/2;
+                    } else {
+                        area = (base * altura) / 2;
                         result.setText(String.valueOf(area));
                     }
                     break;
                 case 3:
-                    if (radio1.length()==0){
+                    if (radio1.length() == 0) {
                         result.setText("no hay valores.");
-                    }
-                    else{
-                        area = 3.159*radio*radio;
+                    } else {
+                        area = 3.159 * radio * radio;
                         result.setText(String.valueOf(area));
                     }
                     break;
